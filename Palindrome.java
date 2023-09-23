846b4370a955cdf8785cee07ba59b23734bd54d1
cafuene <106001676+cafuene@users.noreply.github.com>
2023-09-23 00:45:50 +0000

    GravitationalForce

---
+++ Palindrome.java
@@ -0,0 +1,22 @@
+/*
+ * Write the Palindrome program inside the main method
+ * according to the assignment description.
+ * 
+ * To compile:
+ *        javac Palindrome.java
+ * To execute:
+ *        java Palindrome 5 4 6 6 4 5
+ * 
+ * DO NOT change the class name
+ * DO NOT use System.exit()
+ * DO NOT change add import statements
+ * DO NOT add project statement
+ * 
+ */
+
+public class Palindrome {
+    public static void main(String[] args) {
+       
+        // WRITE YOUR CODE HERE
+    }
+}
