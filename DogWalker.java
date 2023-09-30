aa12e997f906421936e61e79fba5cd8f3a34a5f5
cafuene <106001676+cafuene@users.noreply.github.com>
2023-09-30 01:22:04 +0000

    last

--- DogWalker.java
+++ DogWalker.java
@@ -18,30 +18,29 @@
 public class DogWalker {
     public static void main(String[] args) {
 
-
         int n = Integer.parseInt(args[0]);
         int x = 0;
         int y = 0;
 
-        System.out.println("Step 0: (0, 0)");
+        System.out.println("(0, 0)");
 
-        for (int step = 1; step <= n; step++) {
-            double random = Math.random();
-            if (random < 0.25) {
-                x++; // move east
-            } else if (random < 0.5) {
-                x--; // move west
-            } else if (random < 0.75) {
-                y++; // move north
-            } else {
-                y--; // move south
+        for (int i = 1; i <= n; i++) {
+            double direction = Math.random();
+            if(direction < 0.25){
+                x++; 
+            }else if(direction < 0.5){
+                x--; 
+            }else if(direction < 0.75){
+                y++; 
+            }else{
+                y--; 
             }
 
-            System.out.println("Step " + step + ": (" + x + ", " + y + ")");
+            System.out.println("(" + x + ", " + y + ")");
         }
 
-        double squaredDistance = x * x + y * y;
-        System.out.println("Squared Euclidean distance from the origin: " + squaredDistance);
+        double Distance = x * x + y * y;
+        System.out.println("Squared Distance " + Distance);
     }
 }
 
