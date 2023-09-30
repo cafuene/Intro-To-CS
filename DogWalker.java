90f194cd041181e6de332dff568de8e3e437657b
cafuene <106001676+cafuene@users.noreply.github.com>
2023-09-30 00:49:01 +0000

    sf

--- DogWalker.java
+++ DogWalker.java
@@ -19,7 +19,35 @@ public class DogWalker {
 
     public static void main(String[] args) {
 
-       
+        int n = Integer.parseInt(args[0]);
+        int x = 0;
+        int y = 0;
+        double direction = 0.0;
+
+        for (int i = 0; i <= n; i++) {
+            System.out.println("(" + x +"," + y +")");
+
+            direction = Math.random();
+
+            if(direction <= 0.25){
+                y++;
+
+            }else if(direction >= 0.26 && direction <= 0.5){
+                x++;
+
+            }else if(direction >= 0.51 && direction <= 0.75){
+                y--;
+
+            }else if(direction >= 0.76){
+                x--;
+            }
+
+            
+                
+            }
+            double distance = Math.pow(x, 2) + Math.pow(y, 2);
+           System.out.println("Squared distance =" + distance);
+        }
 			   
     }
-}
+
