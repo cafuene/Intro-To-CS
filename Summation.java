26fb616f5c8d2576e29c5fe4cbf533859b0dfe4b
cafuene <106001676+cafuene@users.noreply.github.com>
2023-10-05 22:24:24 +0000

    Summation

--- Summation.java
+++ Summation.java
@@ -4,7 +4,7 @@ public class Summation {
         int num = Integer.parseInt(args[0]);
         int sum = 0;
 
-        for (int i = 0; i <= num;i++){
+        for (int i = 1; i <= num; i++){
 
             sum = sum + i;
         }
