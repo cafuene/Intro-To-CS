4c3e8a2aeb32b7b84b574d8109c3802ca5b118b1
cafuene <106001676+cafuene@users.noreply.github.com>
2023-10-10 18:29:15 +0000

    end

---
+++ CollectNames.java
@@ -0,0 +1,12 @@
+public class CollectNames {
+    public static void main(String[] args) {
+        
+        StdOut.println("type your first name: ");
+        String x = StdIn.readString();
+        StdOut.println("type your second name: ");
+        String y = StdIn.readString();
+        StdOut.println(x + " " + y);
+        
+
+    }
+}
